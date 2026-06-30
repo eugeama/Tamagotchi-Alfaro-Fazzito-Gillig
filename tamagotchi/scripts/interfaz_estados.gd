325249eec091da088f535c765e6779a5ee720baa
Albano <alumno26.fazzito.albano@ipm.edu.ar>
2026-06-30 10:02:03 -0300

    -Se mejoraron animaciones -Se mejoro sistema de estadisticas -Se añadieron nuevos fondos -Se añadio piso -Se añadio movimiento de mascota

--- tamagotchi/scripts/interfaz_estados.gd
+++ tamagotchi/scripts/interfaz_estados.gd
@@ -1,13 +1,25 @@
 extends Control
 
-@onready var mascota:Node2D = get_node("/root/habitacion/mascota")
+@onready var mascota:Node2D = get_parent()
 @onready var barraHambre:ProgressBar=$VBoxContainer/hambre/ProgressBar
 @onready var barraEnergia:ProgressBar=$VBoxContainer/energia/ProgressBar
 @onready var barraAburrimiento:ProgressBar=$VBoxContainer/aburrimiento/ProgressBar
+@onready var botonJugar:Button=$HBoxContainer/jugar
+@onready var botonComer:Button=$HBoxContainer/comer
+@onready var botonDormir:Button=$HBoxContainer/dormir
 
 func _ready() -> void:
-	EstadoMascota.cambioEstado.connect(actualizarBarras)
+	if not EstadoMascota.cambioEstado.is_connected(actualizarBarras):
+		EstadoMascota.cambioEstado.connect(actualizarBarras)
+	if not botonJugar.pressed.is_connected(_on_jugar_pressed):
+		botonJugar.pressed.connect(_on_jugar_pressed)
+	if not botonComer.pressed.is_connected(_on_comer_pressed):
+		botonComer.pressed.connect(_on_comer_pressed)
+	if not botonDormir.pressed.is_connected(_on_dormir_pressed):
+		botonDormir.pressed.connect(_on_dormir_pressed)
 	actualizarBarras()
+	
+	
 func actualizarBarras() -> void:
 	barraEnergia.value=EstadoMascota.energia
 	barraHambre.value=EstadoMascota.hambre
