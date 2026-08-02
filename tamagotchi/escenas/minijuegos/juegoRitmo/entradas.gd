a63b2769f97529d3ab99419fa649dbd74ab004da
torialfaro <ALUMNO26.ALFARO.Victoria@ipm.edu.ar>
2026-08-01 22:04:17 -0300

    arreglando error de referencia a escena flechas.tscn

--- tamagotchi/escenas/minijuegos/juegoRitmo/entradas.gd
+++ tamagotchi/escenas/minijuegos/juegoRitmo/entradas.gd
@@ -1,6 +1,6 @@
 extends Sprite2D
 @export var tecla:String=""
-@onready var flecha=preload("res://escenas/minijuegos/juegoRitmo/flechas.gd")
+@onready var flecha=preload("res://escenas/minijuegos/juegoRitmo/flechas.tscn")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func inicio():
 	set_process(false)
@@ -8,6 +8,6 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed(tecla):
 		generarFlecha()
 func generarFlecha():
-	var nuevaFlecha=flecha.new()
+	var nuevaFlecha=flecha.instantiate()
 	get_tree().get_root().call_deferred("add_child",nuevaFlecha)
 	nuevaFlecha.Setup(position.x,frame+4)
