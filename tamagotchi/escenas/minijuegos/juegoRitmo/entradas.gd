f6c987084f80bd12bfd3de051bd30cdc0328ca24
torialfaro <alumno26.alfaro.victoria@ipm.edu.ar>
2026-08-04 17:46:47 -0300

    empezando scores diferentes + animaciones cuando se hace hit en una flecha

--- tamagotchi/escenas/minijuegos/juegoRitmo/entradas.gd
+++ tamagotchi/escenas/minijuegos/juegoRitmo/entradas.gd
@@ -2,6 +2,16 @@ extends Sprite2D
 @export var tecla:String=""
 @onready var flecha=preload("res://escenas/minijuegos/juegoRitmo/flechas.tscn")
 var listaFlechas=[]
+var distPerfecto:float=30
+var distGenial:float=40
+var distBien:float=50
+var distOk: float=60
+
+var perfectoScr:int=300
+var genialScr:int=200
+var bienScr:int=100
+var okScr:int=50
+
 func inicio():
 	set_process(false)
 func _process(delta: float) -> void:
@@ -11,11 +21,25 @@ func _process(delta: float) -> void:
 		if Input.is_action_just_pressed(tecla):
 			var flechaHit=listaFlechas.front()
 			var distancia=abs(global_position.y-flechaHit.global_position.y)
-			var margen=40.0
-			if distancia<=margen:
+			if distancia<distPerfecto:
+				Seniales.aumentarPuntaje.emit(perfectoScr)
 				listaFlechas.pop_front()
-				Seniales.aumentarPuntaje.emit(100)
 				flechaHit.queue_free()
+			elif distancia<distGenial:
+				Seniales.aumentarPuntaje.emit(genialScr)
+				listaFlechas.pop_front()
+				flechaHit.queue_free()
+			elif distancia<distBien:
+				Seniales.aumentarPuntaje.emit(bienScr)
+				listaFlechas.pop_front()
+				flechaHit.queue_free()
+			elif distancia<distOk:
+				Seniales.aumentarPuntaje.emit(okScr)
+				listaFlechas.pop_front()
+				flechaHit.queue_free()
+			else:
+				pass
+				
 func generarFlecha():
 	var nuevaFlecha=flecha.instantiate()
 	get_tree().get_root().call_deferred("add_child",nuevaFlecha)
