d9386b6d081a2c3750762e95059f3f20e13ff764
abriltedesco <alumno26.tedesco.abril@ipm.edu.ar>
2026-08-11 16:20:30 -0300

    leyendas de cada hit + fade out

--- tamagotchi/escenas/minijuegos/juegoRitmo/entradas.gd
+++ tamagotchi/escenas/minijuegos/juegoRitmo/entradas.gd
@@ -1,6 +1,7 @@
 extends Sprite2D
 @export var tecla:String=""
 @onready var flecha=preload("res://escenas/minijuegos/juegoRitmo/flechas.tscn")
+@onready var popUp=preload("res://escenas/minijuegos/juegoRitmo/scorePop.tscn")
 var listaFlechas=[]
 var distPerfecto:float=30
 var distGenial:float=40
@@ -12,34 +13,52 @@ var genialScr:int=200
 var bienScr:int=100
 var okScr:int=50
 
+func ready():
+	$brillo.frame=frame+4
 func inicio():
 	set_process(false)
 func _process(delta: float) -> void:
 	if !listaFlechas.is_empty():
 		if listaFlechas.front().fuera:
 			listaFlechas.pop_front()
+			var popUpPuntaje=popUp.instantiate()
+			get_tree().get_root().call_deferred("add_child", popUpPuntaje)
+			popUpPuntaje.setInfoPuntaje("...")
+			popUpPuntaje.global_position=global_position+Vector2(0,-20)
 		if Input.is_action_just_pressed(tecla):
 			var flechaHit=listaFlechas.front()
 			var distancia=abs(global_position.y-flechaHit.global_position.y)
+			$AnimationPlayer.stop()
+			$AnimationPlayer.play()
+			var leyendaScore:String=" "
 			if distancia<distPerfecto:
 				Seniales.aumentarPuntaje.emit(perfectoScr)
+				leyendaScore="PERFECTO"
 				listaFlechas.pop_front()
 				flechaHit.queue_free()
 			elif distancia<distGenial:
 				Seniales.aumentarPuntaje.emit(genialScr)
+				leyendaScore="GENIAL"
 				listaFlechas.pop_front()
 				flechaHit.queue_free()
 			elif distancia<distBien:
 				Seniales.aumentarPuntaje.emit(bienScr)
+				leyendaScore="BIEN"
 				listaFlechas.pop_front()
 				flechaHit.queue_free()
 			elif distancia<distOk:
 				Seniales.aumentarPuntaje.emit(okScr)
+				leyendaScore="OK"
 				listaFlechas.pop_front()
 				flechaHit.queue_free()
 			else:
-				pass
+				leyendaScore="..."
 				
+			var popUpPuntaje=popUp.instantiate()
+			get_tree().get_root().call_deferred("add_child", popUpPuntaje)
+			popUpPuntaje.setInfoPuntaje(leyendaScore)
+			popUpPuntaje.global_position=global_position+Vector2(0,-20)
+			
 func generarFlecha():
 	var nuevaFlecha=flecha.instantiate()
 	get_tree().get_root().call_deferred("add_child",nuevaFlecha)
--- tamagotchi/escenas/minijuegos/juegoRitmo/scorePop.gd
+++ tamagotchi/escenas/minijuegos/juegoRitmo/scorePop.gd
@@ -1,11 +1,21 @@
 extends Control
 
+func setInfoPuntaje(texto:String):
+	$RichTextLabel.text="[center]"+texto
+	match texto:
+		"PERFECTO":
+			$RichTextLabel.set("theme_override_colors/font_outline_color", Color("e952daff"))
+			$RichTextLabel.set("theme_override_colors/default_color", Color("c7acd4"))
+		"GENIAL":
+			$RichTextLabel.set("theme_override_colors/font_outline_color", Color("2d9b87ff"))
+			$RichTextLabel.set("theme_override_colors/default_color", Color("6de7d9ff"))
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+		"BIEN":
+			$RichTextLabel.set("theme_override_colors/font_outline_color", Color("2873afff"))
+			$RichTextLabel.set("theme_override_colors/default_color", Color("77b7f2ff"))
+		"OK":
+			$RichTextLabel.set("theme_override_colors/font_outline_color", Color("a07239ff"))
+			$RichTextLabel.set("theme_override_colors/default_color", Color("c89352ff"))
+		_:
+			$RichTextLabel.set("theme_override_colors/font_outline_color", Color("000000ff"))
+			$RichTextLabel.set("theme_override_colors/default_color",Color("ffffffff"))
